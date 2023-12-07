2454a4d00e69b5326c67f99d18d0a4a09ed3247d
SarthakDeshmukh13 <saksard@gmail.com>
2023-12-07 15:38:08 +0530

    Mantine Setup

---
+++ government/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "@mantine/core";
+import "./App.css";
+import "@mantine/core/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="filled" color="violet" size="md">
+        Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
