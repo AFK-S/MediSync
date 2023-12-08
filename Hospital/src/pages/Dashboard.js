3e812d4105863f8611e8891b7b008fddf137eab5
Karan2310 <karandeep9619@gmail.com>
2023-12-09 00:38:57 +0530

    Dashboard Updated

--- Hospital/src/pages/Dashboard.js
+++ Hospital/src/pages/Dashboard.js
@@ -1,8 +1,177 @@
-import React from "react";
+import React, { useState } from "react";
 import { Divider, Group, Text } from "@mantine/core";
 import Chart from "react-apexcharts";
 
+const Table = ({ data, columns }) => {
+  return (
+    <div
+      className="inner-container"
+      style={{ overflowY: "auto", maxHeight: "40vh" }}
+    >
+      <table className="table table-hover text-no-wrap">
+        <thead>
+          <tr>
+            {columns.map((col, index) => (
+              <th key={index} scope="col" className="text-no-wrap">
+                {col}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((item, index) => (
+              <tr key={index}>
+                {columns.map((col, colIndex) => (
+                  <td key={colIndex} style={{ whiteSpace: "nowrap" }}>
+                    {item[col.toLowerCase()]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 const Dashboard = () => {
+  const [doctors, setSoctors] = useState([
+    {
+      name: "Karandeep Singh Sandhu",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+    {
+      name: "Karan",
+      age: "40",
+      experience: "10",
+      specialty: "cardiologist",
+      date: "18/10/2023",
+      timeslot: "12:00pm - 03:00pm",
+    },
+  ]);
+
+  const [patientsWaiting, setPatientsWaiting] = useState([
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+    {
+      name: "Karan",
+      doctor: "Dr. XYZ",
+      timeslot: "10:00am - 10:30am",
+      date: "23/10/2023",
+    },
+  ]);
+
   const bedsChartData = {
     series: [40, 60],
     options: {
@@ -69,6 +238,31 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Available Doctors */}
+      <div className="container-fluid c-card my-4">
+        <h4 className="mb-2">Doctors Available</h4>
+        <Table
+          data={doctors}
+          columns={[
+            "Name",
+            "Age",
+            "Experience",
+            "Specialty",
+            "Date",
+            "TimeSlot",
+          ]}
+        />
+      </div>
+
+      {/* Waiting Patients */}
+      <div className="container-fluid c-card my-4">
+        <h4 className="mb-2">Patients Waiting</h4>
+        <Table
+          data={patientsWaiting}
+          columns={["Name", "Date", "Doctor", "TimeSlot"]}
+        />
       </div>
     </div>
   );
