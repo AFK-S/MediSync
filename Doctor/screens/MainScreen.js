8f4cf0632ed53649046da34a1cb1ccc97dfefc58
Karan2310 <karandeep9619@gmail.com>
2023-12-11 01:46:33 +0530

    Icons Added

--- Doctor/screens/MainScreen.js
+++ Doctor/screens/MainScreen.js
@@ -2,45 +2,78 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useStateContext } from "../context/StateContext";
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => (
+const PatientScreen = () => (
   <View>
-    <Text>Home Screen</Text>
+    <Text>Patient Screen</Text>
   </View>
 );
 
-const ProfileScreen = () => (
-  <View>
-    <Text>Profile Screen</Text>
-  </View>
-);
-
-const SettingsScreen = () => (
-  <View>
-    <Text>Settings Screen</Text>
-  </View>
-);
-
-const MainScreen = () => {
+const ProfileScreen = () => {
   const { setLogin } = useStateContext();
 
-  const handleLogout = () => {
-    // Set isLogin to false
+  const handleLogout = async () => {
+    try {
+      // Clear async storage if needed
+      await AsyncStorage.removeItem("isLogin");
+    } catch (error) {
+      console.error("Error removing login status:", error);
+    }
     setLogin(false);
   };
 
   return (
+    <View>
+      <Text>Profile Screen</Text>
+      <Button title="Logout" onPress={handleLogout} />
+    </View>
+  );
+};
+
+const AppointmentScreen = () => (
+  <View>
+    <Text>Appointment Screen</Text>
+  </View>
+);
+
+const MainScreen = () => {
+  return (
     <Tab.Navigator
-      screenOptions={{
-        headerShown: false, // Hide the header
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Patient") {
+            iconName = focused ? "account-heart" : "account-heart-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "account" : "account-outline";
+          } else if (route.name === "Appointment") {
+            iconName = focused ? "calendar" : "calendar-blank-outline";
+          }
+
+          return (
+            <MaterialCommunityIcons name={iconName} size={size} color={color} />
+          );
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#4CAF50",
+        labelStyle: {
+          fontSize: 10,
+        },
+        iconStyle: {
+          marginTop: 10,
+        },
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Patient" component={PatientScreen} />
+      <Tab.Screen name="Appointment" component={AppointmentScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
   );
 };
