92ca55363b61a6928be54043a959ebe5782e2cd6
adi50920 <aaditya.rai@dmce.ac.in>
2023-12-12 22:49:02 +0530

    Login Page Created

--- Doctor/screens/auth/LoginScreen.js
+++ Doctor/screens/auth/LoginScreen.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Image,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useStateContext } from "../../context/StateContext";
 
@@ -27,12 +36,87 @@ const LoginScreen = ({ navigation }) => {
   };
 
   return (
-    <View>
-      <Text>Login</Text>
-      <Button title="Login" onPress={handleLogin} />
-      <Button title="Go to Register" onPress={handleGoToRegister} />
-    </View>
+    <>
+      {/* <Image
+        source={require("../../assets/logo.png")}
+        style={{ width: 150, height: 150, borderRadius: 100 }}
+      /> */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.innerContainer}
+      >
+        <View style={styles.container}>
+          <Text style={styles.title}>Login</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Username or Email"
+            // Add onChangeText prop to handle input changes
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            // Add onChangeText prop to handle input changes
+          />
+          <TouchableOpacity
+            style={styles.button}
+            title="Login"
+            onPress={handleLogin}
+          >
+            <Text style={styles.btnText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            title="Go to Register"
+            onPress={handleGoToRegister}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center", // Center vertically
+    alignItems: "center",
+  },
+  container: {
+    backgroundColor: "#fff",
+    padding: 40,
+    marginBottom: 80,
+    borderRadius: 30,
+  },
+
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 50,
+    borderColor: "gray",
+    borderRadius: 15,
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 12,
+    width: 300,
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "#18C37D",
+    height: 50,
+    color: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    marginTop: 30,
+  },
+  btnText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
 
 export default LoginScreen;
