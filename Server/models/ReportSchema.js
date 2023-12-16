98942f55ebdc048e0e70c2e81946d1bd936fbe96
FaizanPotrick <faizan.potrick02@gmail.com>
2023-12-16 15:14:26 +0530

    Add report schema

---
+++ Server/models/ReportSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const { Schema, connection } = mongoose;
+
+const ReportSchema = new Schema(
+  {
+    patient_id: {
+      type: Schema.Types.ObjectId,
+      required: [true, "Please provide a Patient ID"],
+    },
+    type: {
+      type: String,
+      trim: true,
+      enum: ["patient", "doctor"],
+      match: [
+        /^[a-zA-Z]+$/,
+        (props) => `${props.value} is not a valid report type`,
+      ],
+      required: [true, "Please provide the Report Type"],
+    },
+    disease: {
+      type: Array,
+      trim: true,
+      required: [true, "Please provide the Disease"],
+    },
+    url: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a Report URL"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default connection.useDb("MediSync").model("Report", ReportSchema);
